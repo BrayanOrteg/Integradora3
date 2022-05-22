f9d54c7dfdc76f01df38d418ca79530f1d97cbfc
BrayanOrteg <brayansteven0322@gmail.com>
2022-05-22 00:54:34 -0500

    Upload adjustment in the init

--- src/ui/Init.java
+++ src/ui/Init.java
@@ -38,7 +38,7 @@ public class Init {
         Vehicule newVehicule = new Gasoline(1011,1000, 2000, "Nissan", 2014, 2000, 1200, TYPEVEHICULE.USED, "AB3-203", propertyCard1, soat1, mechanicalReview1, TYPEAUTOMOBILE.SEDAN, 4, "yes", 30, TYPEGASOLINE.CURRENT, 5);
         vehicules.add(newVehicule);
 
-        Vehicule newVehicule2= new Electric(1330,1000, 2000, "Tesla", 2012, 2400, 1000, TYPEVEHICULE.USED, "AAD-003", propertyCard2, soat2, mechanicalReview2, TYPEAUTOMOBILE.SEDAN, 4, "yes", TYPECHARGER.FAST, 7, 1);
+        Vehicule newVehicule2= new Electric(1330,1000, 2000, "Tesla", 2012, 2400, 1000, TYPEVEHICULE.NEW, "AAD-003", propertyCard2, soat2, mechanicalReview2, TYPEAUTOMOBILE.SEDAN, 4, "yes", TYPECHARGER.FAST, 7, 1);
         vehicules.add(newVehicule2);
 
         Vehicule newVehicule3= new Hybrid(1739,3000, 2000, "Tesla", 2013, 2200, 1400, TYPEVEHICULE.USED, "KEN-110", propertyCard3, soat3, mechanicalReview3, TYPEAUTOMOBILE.PICKUP, 4, "yes", 35, TYPEGASOLINE.EXTRA, 4, TYPECHARGER.NORMAL, 4, 2);
